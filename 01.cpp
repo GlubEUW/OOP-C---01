70a05d48fdd05bf3bfb7b73ecb8c2a8ca41b2776
GlubEUW <mindaugas.kalvinskas@gmail.com>
2025-02-22 12:06:14 +0200

    Iniatial class and toString()

--- 01.cpp
+++ 01.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
+class Animal{
 
+    string name;
+    int age, legCount;
+    bool status;
+
+    string toString(){
+        stringstream ss;
+        ss << name << " "<< age << " " << legCount << " " << status;
+        return ss.str();
+    }
+};
 
 int main(){
 
