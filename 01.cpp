2c033b98bef59aecc01b76a164a250c0e7a34144
GlubEUW <mindaugas.kalvinskas@gmail.com>
2025-02-22 12:27:24 +0200

    Getters, setters

--- 01.cpp
+++ 01.cpp
@@ -4,21 +4,36 @@
 using namespace std;
 
 class Animal{
+    Animal(){
+            status = true;
+        }
 
-    string name;
-    int age, legCount;
-    bool status;
+    private:
+        string name;
+        int age, legCount;
+        bool status;
 
-    string toString(){
-        stringstream ss;
-        ss << name << " "<< age << " " << legCount << " " << status;
-        return ss.str();
-    }
+    public:
+
+        void setName(string n){name = n;}
+        string getName(){return name;}
+
+        void setAge(int a){age = a;}
+        int getAge(){return age;}
+
+        void setLegCount(int l){legCount = l;}
+        int getLegCount(){return legCount;}
+
+        bool isDead(){return status;}
+        void dead(){status = 0;}
+        void alive(){status = 1;}
+        string toString(){
+            stringstream ss;
+            ss << name << " "<< age << " " << legCount << " " << status;
+            return ss.str();
+        }
 };
 
 int main(){
-
-
-
 
 }
