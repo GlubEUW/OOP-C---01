87654c5c137308480050df6bfad7559ecfd188a7
GlubEUW <mindaugas.kalvinskas@gmail.com>
2025-02-28 10:25:05 +0200

    Fixed indentation

--- 01.cpp
+++ 01.cpp
@@ -89,77 +89,77 @@ int main(){
     try{
 
 
-    const int amount = 1000;
-    Animal cow(12,4);
+        const int amount = 1000;
+        Animal cow(12,4);
 
 
 
-    assert(cow.getAge() == 12);
-    assert(cow.getLegCount() == 4);
+        assert(cow.getAge() == 12);
+        assert(cow.getLegCount() == 4);
 
-    cout << "Constructor check passed" << endl;
+        cout << "Constructor check passed" << endl;
 
-    cow.setLegCount(3);
-    assert(cow.getLegCount() == 3);
+        cow.setLegCount(3);
+        assert(cow.getLegCount() == 3);
 
-    cow.setAge(cow.getAge() + 1);
-    assert(cow.getAge() == 13);
+        cow.setAge(cow.getAge() + 1);
+        assert(cow.getAge() == 13);
 
-    cow.setName("Moo");
-    assert(cow.getName() == "Moo");
-    cout << "Set and Get name check passed" << endl;
+        cow.setName("Moo");
+        assert(cow.getName() == "Moo");
+        cout << "Set and Get name check passed" << endl;
 
-    cow.setLegCount(3);
-    assert(cow.getLegCount() == 3);
-    cout << "Set and Get leg count check passed" << endl;
+        cow.setLegCount(3);
+        assert(cow.getLegCount() == 3);
+        cout << "Set and Get leg count check passed" << endl;
 
-    cow.dead();
-    assert(!cow.getIsAlive());
-    cout << "Animal death check passed rip" << endl;
-
-
-    assert(cow.toString() == "Moo 13 3 0");
-    cout << "To string check passed" << endl;
+        cow.dead();
+        assert(!cow.getIsAlive());
+        cout << "Animal death check passed rip" << endl;
 
 
-    Animal* animals[amount];
-    // example: let's say we need to sort thorough data and find if some animals are dead later
-    for (int i = 0; i < amount; i++) {
-        animals[i] = new Animal(i%10, 4, "Cow" + to_string(i + 1));
-        if (animals[i]->getAge() >= 5)
-            animals[i]->dead();
-    }
-
-    assert(!animals[6]->getIsAlive());
-    assert(!animals[16]->getIsAlive());
-    assert(!animals[126]->getIsAlive());
-    assert(!animals[996]->getIsAlive());
+        assert(cow.toString() == "Moo 13 3 0");
+        cout << "To string check passed" << endl;
 
 
-    cout << "Is dead on generated list test passed" << endl;
+        Animal* animals[amount];
+        // example: let's say we need to sort thorough data and find if some animals are dead later
+        for (int i = 0; i < amount; i++) {
+            animals[i] = new Animal(i%10, 4, "Cow" + to_string(i + 1));
+            if (animals[i]->getAge() >= 5)
+                animals[i]->dead();
+        }
+
+        assert(!animals[6]->getIsAlive());
+        assert(!animals[16]->getIsAlive());
+        assert(!animals[126]->getIsAlive());
+        assert(!animals[996]->getIsAlive());
 
 
-
-    assert(Animal::getCount() == 1001);
-    cout << "Object count check passed" << endl;
-
-    assert(cow.getId() == 0);
-    assert(animals[12]->getId() == 13);
-    assert(animals[999]->getId() == 1000);
-
-    cout << "Id check passed" << endl;
+        cout << "Is dead on generated list test passed" << endl;
 
 
 
+        assert(Animal::getCount() == 1001);
+        cout << "Object count check passed" << endl;
+
+        assert(cow.getId() == 0);
+        assert(animals[12]->getId() == 13);
+        assert(animals[999]->getId() == 1000);
+
+        cout << "Id check passed" << endl;
 
 
-    for (int i = 0; i < amount; i++) {
-        delete animals[i];
-    }
+
+
+
+        for (int i = 0; i < amount; i++) {
+            delete animals[i];
+        }
 
     }
     catch (int age){
-            cout << "Age must be positive" << endl;
+        cout << "Age must be positive" << endl;
     }
     assert(Animal::getCount() == 0);
     cout << "Object delete count check passed" << endl;
